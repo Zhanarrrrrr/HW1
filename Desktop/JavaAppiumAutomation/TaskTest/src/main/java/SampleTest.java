1d7693ef8f473331651e0d254b1e75f3138c40df
Zhanar <zhanara.salamatovna56@gmail.com>
2024-10-16 23:01:06 +0500

    Ex3: Тест: отмена поиска

--- Desktop/JavaAppiumAutomation/TaskTest/src/main/java/SampleTest.java
+++ Desktop/JavaAppiumAutomation/TaskTest/src/main/java/SampleTest.java
@@ -1,8 +1,7 @@
 import io.appium.java_client.android.AndroidDriver;
-import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -33,52 +32,81 @@ public class SampleTest {
 
 
     @Test
-    public void Test() {
+    public void fillAndSearch() {
         waitForElementAndClick(
-                "fragment_onboarding_skip_button",
+                By.id("fragment_onboarding_skip_button"),
                 "Cannot skip the disclaimer page"
         );
 
         waitForElementAndClick(
-                "//*[@resource-id='org.wikipedia:id/search_container']//*[@text='Search Wikipedia']",
+                By.xpath(
+                        "//*[@resource-id='org.wikipedia:id/search_container']//*[@text='Search Wikipedia']"),
                 "Cannot find label of search"
         );
 
-        WebElement searchInput = waitForElementPresent(
-                "org.wikipedia:id/search_src_text",
-                "Cannot find input line"
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/search_src_text"),
+                "music",
+                "No result for search 'music'",
+                15
         );
 
-        String searchTitle = searchInput.getAttribute("text");
-
-        Assert.assertEquals(
-                "Field names are not equal",
-                "Search Wikipedia",
-                searchTitle
+        waitForElementPresent(
+                By.xpath(
+                        "//*[@resource-id='org.wikipedia:id/search_results_list']//*[@text='Musical theatre']"),
+                "Article about 'Musical theatre' is not found",
+                15
         );
     }
 
+    @Test
+    public void clearSearchAndCheck() {
+        clearSearch(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Cannot clear search line",
+                10
+        );
+        waitForElementNotPresent(
+                By.xpath("//*[@resource-id='org.wikipedia:id/search_results_list']//*[@text='Musical theatre']"),
+                "Results are still on page",
+                10);
+    }
 
-    private WebElement waitForElementPresent(String xpath, String error_message, long timeoutInSeconds) {
+
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
         wait.withMessage(error_message + "\n");
-        By by = By.xpath(xpath);
         return wait.until(ExpectedConditions.presenceOfElementLocated(by)
-
         );
     }
 
-    private WebElement waitForElementPresent(String xpath, String error_message) {
-        return waitForElementPresent(xpath, error_message, 10);
-    }
 
-
-    private void waitForElementAndClick(String xpath, String error_message) {
-        WebElement element = waitForElementPresent(xpath, error_message, 10);
+    private void waitForElementAndClick(By by, String error_message) {
+        WebElement element = waitForElementPresent(by, error_message, 10);
         element.click();
     }
 
-    @After
+    private WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
+    }
+
+    private WebElement clearSearch(By by, String error_message, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.clear();
+        return element;
+    }
+
+    private boolean waitForElementNotPresent(By xpath, String error_message, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutInSeconds));
+        wait.withMessage(error_message + "\n");
+        return wait.until(
+                ExpectedConditions.invisibilityOfElementLocated(xpath)
+        );
+    }
+
+    @AfterAll
     public void tearDown() {
         driver.quit();
     }
